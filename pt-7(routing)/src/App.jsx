99dca3d1b74b7e93e10707ca3f200e8afa3ce4ca
Ashutosh Mohanty <94921263+ashutosh-mohanty05@users.noreply.github.com>
2024-03-20 18:11:48 +0530

    Add files via upload

---
+++ pt-7(routing)/src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Login from './components/Login'
+import About from './components/About'
+import User from './components/User'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+function App() {
+  const router= createBrowserRouter([
+    {
+      path:"/",
+      element:<><Navbar /><Home /></>
+    },
+    {
+      path:"/Login",
+      element:<><Navbar /><Login /></>
+    },
+    {
+      path:"/About",
+      element:<><Navbar /><About /></>
+    },
+    {
+      path:"/user/:username",
+      element:<><Navbar/><User/></>
+    },
+  ])
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
+}
+
+export default App
---
+++ pt-8(useContext)/public/without contextAPI/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Components1 from './Components1'
+const Button = ({count}) => {
+  return (
+    <div>
+        <button><span><Components1 count={count}/></span>I am a button</button>
+    </div>
+  )
+}
+
+export default Button
